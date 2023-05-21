69cdf1c08f67cab749c740ba9f311bab88548d28
Milan Sony <milanfrom2001@gmail.com>
2023-05-21 17:14:54 +0530

    code updated

--- codey.py
+++ codey.py
@@ -1,31 +1,45 @@
 import pyaudio
 import speech_recognition as sr
 from time import sleep
+from playsound import playsound
 
 def listen_for_hotword():
   recognizer = sr.Recognizer()
-  with sr.Microphone(device_index=0) as source:
-    print("Listening...")
-    audio = recognizer.listen(source)
-  try:
-    # Google Speech Recognition engine
-    hotword = recognizer.recognize_google(audio)
-    if hotword == "hello":
-      wake_up_assistant()
-  except sr.UnknownValueError:
-    sleep(10)
-    listen_for_hotword()
-
-def wake_up_assistant():
-  recognizer = sr.Recognizer()
-  with sr.Microphone(device_index=0) as source:
-    print("I am here, How can I help you?")
-    audio = recognizer.listen(source)
+  done = False
+  while not done:
     try:
-      commamd = recognizer.recognize_google(audio)
-      print("You have said: "+ commamd)
+      with sr.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+        print("Listening...")
+        playsound('./rec_start.wav')
+        audio = recognizer.listen(mic)
+        recog_audio= recognizer.recognize_google(audio)
+        hotword = recog_audio.lower()
+        if hotword == "hello":
+          wakeup_assistant()
+        else:
+          listen_for_hotword()
     except sr.UnknownValueError:
-      pass
+      listen_for_hotword()
+
+def wakeup_assistant():
+  recognizer = sr.Recognizer()
+  done = False
+  while not done:
+    try:
+      with sr.Microphone() as mic:
+        recognizer.adjust_for_ambient_noise(mic, duration=0.2)
+        print("I am here, How can I help you?")
+        audio = recognizer.listen(mic)
+        recog_audio= recognizer.recognize_google(audio)
+        hotword = recog_audio.lower()
+        print("You have said: "+hotword)
+        playsound('./rec_stop.wav')
+        listen_for_hotword()
+    except sr.UnknownValueError:
+      listen_for_hotword()
 
 while True:
   listen_for_hotword()
+
+
