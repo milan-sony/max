199a306e6cc32835b6bb13b7ee7083650508d865
Milan Sony <milanfrom2001@gmail.com>
2023-05-21 13:08:22 +0530

    mic_id.py code modified

--- mic_id.py
+++ mic_id.py
@@ -1,9 +1,7 @@
 # print all the microphones connected to your machine
-import speech_recognition as s_r
-for lists in s_r.Microphone.list_microphone_names():
-  print(lists) 
-
-# 2nd method
 from pvrecorder import PvRecorder
-for index, device in enumerate(PvRecorder.get_audio_devices()):
-  print(f"[{index}] {device}")
+def mic_id():
+  for index, device in enumerate(PvRecorder.get_audio_devices()):
+    print(f"NAME: {device}, ID: {index}")
+
+mic_id()
