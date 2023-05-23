94a65939863a64e254d6af5a19352cd420e6960c
Milan Sony <milanfrom2001@gmail.com>
2023-05-23 20:10:06 +0530

    new function added

--- functions.py
+++ functions.py
@@ -3,8 +3,8 @@ from playsound import playsound
 from time import sleep
 import webbrowser
 import pywhatkit as pywkt
+from AppOpener import open, close
 
-# python tts
 def ttspeech(speech_text):
   ttsengine = pyttsx3.init()
   ttsengine.setProperty('rate', 160)
@@ -17,22 +17,28 @@ def search_google(search_item):
   ttspeech("Searching" +search_item+ "on google")
   pywkt.search(search_item)
 
-def open_url(url):
+def open_urls(url):
   webbrowser.open_new_tab(url)
   sleep(1)
   playsound('./rec_stop.wav')
 
-def open_youtube(command):
-  ttspeech(command)
-  url = "https://www.youtube.com/"
-  open_url(url)
+def open_applications(command):
+  ttspeech("Opening" +command)
+  open(command)
+  playsound('./rec_stop.wav')
 
-def open_whatsapp(command):
-  ttspeech(command)
-  url = "https://web.whatsapp.com/"
-  open_url(url)
-
-def open_instagram(command):
-  ttspeech(command)
-  url = "https://www.instagram.com/"
-  open_url(url)
+def open_in_browser(command):
+  if ("instagram" in command):
+    ttspeech("Opening" +command)
+    url = "https://www.instagram.com/"
+    open_urls(url)
+  elif ("youtube" in command):
+    ttspeech("Opening" +command)
+    url = "https://www.youtube.com/"
+    open_urls(url)
+  elif ("whatsapp" in command) or ("whatsappweb" in command) or ("whatsapp web" in command):
+    ttspeech("Opening" +command)
+    url = "https://web.whatsapp.com/"
+    open_urls(url)
+  else:
+    pass
